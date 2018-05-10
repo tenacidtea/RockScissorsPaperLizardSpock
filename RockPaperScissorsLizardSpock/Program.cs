349ee79f726369f119810ac0d4ac889f650bb51d
Victor D. Verdev <tenacidtea@gmail.com>
2018-05-09 20:56:20 -0500

    End of Day 20180509

--- RockPaperScissorsLizardSpock/Program.cs
+++ RockPaperScissorsLizardSpock/Program.cs
@@ -10,6 +10,10 @@ namespace RockPaperScissorsLizardSpock
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("\n\nWelcome to the Rock, Paper, Scissors, Lizard, Spock Game!");
+            Console.WriteLine("\n\nPlease choose the number of Players:");
+            Console.SetCursorPosition(36, 5);
+            string numberOfPlayers = Console.ReadLine();
         }
     }
 }
